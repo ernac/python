c602123ffc775fba01cb60bdc19c40e288df48e2
Elias <etunon@gmail.com>
2019-07-21 15:07:30 -0300

    Changes

--- Machine Learning/1. ML_Data_Preprocessing.py
+++ Machine Learning/1. ML_Data_Preprocessing.py
@@ -4,4 +4,16 @@
 # ## Data Preprocessing
 # ###
 
+# Importing libraries
+
+# numpy, matplotlib e pandas
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importando o conjunto de dados
+dataset = pd.read_csv('Data.csv')
+dataset
+
 
