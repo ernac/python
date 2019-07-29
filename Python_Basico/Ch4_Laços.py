418347bc9b11bf95e4daba24dfb859ed07d5bc62
Elias <etunon@gmail.com>
2019-07-29 20:57:35 -0300

    Finalizado Chapter 04

--- Python_Basico/Ch4_Laços.py
+++ Python_Basico/Ch4_Laços.py
@@ -116,5 +116,105 @@ classes.sort() #ordenando a lista em ordem alfabética
 classes #lista ordenada
 classes [0:4] # uma fatia de um intervalo da lista
 
+# sem declarar a porção inicial
 
+classes [:5]
+
+# sem declarar a porção final 
+
+classes[0:]
+
+# Essa sintaxe permite apresentar todos os elementos a partir de qualquer ponto de sua lista até o final, independentemente do tamanho da lista.
+
+
+## Índices negativos
+
+# Exibir o último item apenas
+classes[-1:]
+
+# Exibir os dois últimos itens da lista
+classes[-2:]
+
+
+#%%
+
+# Percorrendo as fatias com laços
+
+# Exibir os nomes das classes em formato de laço
+
+for i in classes:
+    print i.title() + ' ' + 'é uma classe de D&D'
+print 'Estas são todas as classes deste exemplo.'
+
+
+# Tirando cópia de uma lista
+
+# Comiga predileta
+
+rango_1 = ['pizza', 'churrasco', 'bolo de cenoura']
+rango_2 = rango_1 [:]
+print rango_2
+
+#%%
+
+# Exercícios
+
+# Fatias: Usando um dos programas que você escreveu neste capítulo,acrescente várias linhas no final do programa que façam o seguinte: 
+# • Exiba a mensagem Os três primeiros itens da lista são: Em seguida, use uma fatia para exibir os três primeiros itens da lista desse programa. 
+# • Exiba a mensagem Três itens do meio da lista são:. Use uma fatia para exibir três itens do meio da lista. 
+# • Exiba a mensagem Os três últimos itens da lista são:. Use uma fatia para exibir os três últimos itens da lista. 
+
+mult5 = [i+2 for i in range(0,10,2)]
+mult5
+
+# 3 primeiros
+mult5[0:3]
+# 3 do meio
+mult5[1:4]
+# 3 finais
+mult5[-3:]
+
+# 4.11 – Crie uma lista de pizzas favoritas com cinco sabores. Faça uma cópia da lista de pizzas e chame-a de friend_pizzas. Então faça o seguinte: 
+# • Adicione uma nova pizza à lista original. 
+# • Adicione uma pizza diferente à lista friend_pizzas. 
+# • Prove que você tem duas listas diferentes. Exiba a mensagem Minhas pizzas favoritas são:; em seguida, utilize um laço for para exibir a primeira lista. Exiba a mensagem As pizzas favoritas de meu amigo são:; em seguida, utilize um laço for para exibir a segunda lista. Certifique-se de que cada pizza nova esteja armazenada na lista apropriada.
+
+pizzas1 = ['calabresa','siciliana','banana','portuguesa','quatro queijos']
+pizzas2 = pizzas1[:]
+
+pizzas1.append('toscana')
+pizzas1
+
+pizzas2.append('pepperoni')
+pizzas2
+
+for i in pizzas1:
+    print 'As melhores pizzas sao ' + i.title()
+
+for j in pizzas2:
+    print 'Tambem gosto de ' + j.title()
+
+#%%
+
+# Tuplas
+
+# Conceito
+
+# Uma lista cujos os itens não podem ser mudados. As tuplas são definidas com parênteses no lugar de colchetes.
+
+# Tuplas são imutáveis...
+
+dimensao = (100,150) # dimensões de um retângulo, altura e largura
+dimensao[0]
+dimensao[1]
+
+# Tentando atribuir um valor para o índice 0:
+dimensao[0] = 200 # TypeError: 'tuple' object does not support item assignment
+
+# ...porém, podem ser sobrescritas!
+
+dimensao=(150,400)
+dimensao[:]
+
+# Se comparada com listas, as tuplas são estruturas de dados simples. Use-as quando quiser armazenar um conjunto de valores que não deva ser alterado durante a vida de um programa.
 
